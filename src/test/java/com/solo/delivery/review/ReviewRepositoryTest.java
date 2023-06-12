602ff2ce24cdaba5156627707614008bcd786e33
jgb1123 <rmsqja2@gmail.com>
2023-06-12 16:57:47 +0900

    [test] Review Test #30

---
+++ src/test/java/com/solo/delivery/review/ReviewRepositoryTest.java
@@ -0,0 +1,70 @@
+package com.solo.delivery.review;
+
+import com.solo.delivery.dummy.ReviewDummy;
+import com.solo.delivery.dummy.StoreDummy;
+import com.solo.delivery.member.repository.MemberRepository;
+import com.solo.delivery.review.entity.Review;
+import com.solo.delivery.review.repository.ReviewRepository;
+import com.solo.delivery.store.entity.Store;
+import com.solo.delivery.store.repository.StoreRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class ReviewRepositoryTest {
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private StoreRepository storeRepository;
+
+    @Test
+    void saveTest() {
+        Review review = ReviewDummy.createReview1();
+
+        Review savedReview = reviewRepository.save(review);
+
+        assertThat(savedReview.getStar()).isEqualTo(review.getStar());
+        assertThat(savedReview.getReviewContent()).isEqualTo(review.getReviewContent());
+    }
+
+    @Test
+    void findAllByStoreTest() {
+        Review review1 = ReviewDummy.createReview1();
+        Review review2 = ReviewDummy.createReview2();
+        Store store = StoreDummy.createStore1();
+        Store savedStore = storeRepository.save(store);
+        review1.changeStore(savedStore);
+        review2.changeStore(savedStore);
+        Review savedReview1 = reviewRepository.save(review1);
+        Review savedReview2 = reviewRepository.save(review2);
+
+        Page<Review> reviewPage = reviewRepository.findAllByStore(savedStore, PageRequest.of(0, 10, Sort.by("reviewId").ascending()));
+        List<Review> reviews = reviewPage.getContent();
+
+        assertThat(reviews).contains(savedReview1);
+        assertThat(reviews).contains(savedReview2);
+    }
+
+    @Test
+    void updateTest() {
+        Review modifiedReview = ReviewDummy.createReview2();
+        Review review = ReviewDummy.createReview1();
+        Review savedReview = reviewRepository.save(review);
+        Review foundReview = reviewRepository.findById(savedReview.getReviewId()).orElse(null);
+
+        reviewRepository.delete(savedReview);
+        Review foundReviewAfterDelete = reviewRepository.findById(savedReview.getReviewId()).orElse(null);
+
+        assertThat(foundReview).isNotNull();
+        assertThat(foundReviewAfterDelete).isNull();
+    }
+}
---
+++ src/test/java/com/solo/delivery/review/ReviewServiceTest.java
@@ -0,0 +1,102 @@
+package com.solo.delivery.review;
+
+import com.solo.delivery.dummy.MemberDummy;
+import com.solo.delivery.dummy.ReviewDummy;
+import com.solo.delivery.dummy.StoreDummy;
+import com.solo.delivery.member.entity.Member;
+import com.solo.delivery.member.service.MemberService;
+import com.solo.delivery.review.entity.Review;
+import com.solo.delivery.review.repository.ReviewRepository;
+import com.solo.delivery.review.service.ReviewService;
+import com.solo.delivery.store.entity.Store;
+import com.solo.delivery.store.service.StoreService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.*;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class ReviewServiceTest {
+    @InjectMocks
+    private ReviewService reviewService;
+
+    @Mock
+    private ReviewRepository reviewRepository;
+
+    @Mock
+    private StoreService storeService;
+
+    @Mock
+    private MemberService memberService;
+
+    @Test
+    void createReviewTest() {
+        Review review = ReviewDummy.createReview1();
+        Member member = MemberDummy.createMember1();
+        Store store = StoreDummy.createStore1();
+        given(memberService.findVerifiedMember(Mockito.anyString()))
+                .willReturn(member);
+        given(storeService.findVerifiedStore(Mockito.anyLong()))
+                .willReturn(store);
+        given(reviewRepository.save(Mockito.any(Review.class)))
+                .willReturn(review);
+
+        Review savedReview = reviewService.createReview(review, "hgd@gmail.com", 1L);
+
+        assertThat(savedReview.getMember().getEmail()).isEqualTo(member.getEmail());
+        assertThat(savedReview.getStore().getStoreName()).isEqualTo(store.getStoreName());
+        assertThat(savedReview.getReviewContent()).isEqualTo(review.getReviewContent());
+    }
+
+    @Test
+    void findReviewsTest() {
+        Review review1 = ReviewDummy.createReview1();
+        Review review2 = ReviewDummy.createReview2();
+        given(storeService.findVerifiedStore(Mockito.anyLong()))
+                .willReturn(new Store());
+        given(reviewRepository.findAllByStore(Mockito.any(Store.class), Mockito.any(Pageable.class)))
+                .willReturn(new PageImpl<>(List.of(review1, review2), PageRequest.of(0, 10, Sort.by("reviewId").ascending()), 2));
+
+        Page<Review> reviewPage = reviewService.findReviews(1L, 1, 10);
+        List<Review> reviews = reviewPage.getContent();
+
+        assertThat(reviews).contains(review1);
+        assertThat(reviews).contains(review2);
+    }
+
+    @Test
+    void updateReviewTest() {
+        Review modifiedReview = ReviewDummy.createReview2();
+        Review review = ReviewDummy.createReview1();
+        Member member = MemberDummy.createMember1();
+        given(memberService.findVerifiedMember(Mockito.anyString()))
+                .willReturn(member);
+        given(reviewRepository.findById(Mockito.anyLong()))
+                .willReturn(Optional.of(review));
+
+        Review updatedReview = reviewService.updateReview(modifiedReview, 1L, "hgd@gmail.com");
+
+        assertThat(updatedReview.getReviewContent()).isEqualTo(modifiedReview.getReviewContent());
+    }
+
+    @Test
+    void deleteReviewTest() {
+        Review review = ReviewDummy.createReview1();
+        Member member = MemberDummy.createMember1();
+        review.changeMember(member);
+        given(reviewRepository.findById(Mockito.anyLong()))
+                .willReturn(Optional.of(review));
+
+        Assertions.assertDoesNotThrow(() -> reviewService.deleteReview("hgd@gmail.com", 1L));
+    }
+}
