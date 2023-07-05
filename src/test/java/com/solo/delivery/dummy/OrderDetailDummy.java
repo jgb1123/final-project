db8bf7f85deb8654963ed613bc8ce48b73df39a1
jgb1123 <rmsqja2@gmail.com>
2023-07-06 00:09:21 +0900

    [test] OrderService 리팩토링 후 test 수정 #68

--- src/test/java/com/solo/delivery/dummy/OrderDetailDummy.java
+++ src/test/java/com/solo/delivery/dummy/OrderDetailDummy.java
@@ -2,8 +2,29 @@ package com.solo.delivery.dummy;
 
 import com.solo.delivery.order.dto.OrderDetailPostDto;
 import com.solo.delivery.order.dto.OrderDetailResponseDto;
+import com.solo.delivery.order.entity.OrderDetail;
 
 public interface OrderDetailDummy {
+    static OrderDetail createOrderDetail1() {
+        return OrderDetail.builder()
+                .orderDetailId(1L)
+                .itemOrderCnt(1)
+                .itemId(1L)
+                .itemName("김치볶음밥")
+                .itemPrice(8000)
+                .build();
+    }
+
+    static OrderDetail createOrderDetail2() {
+        return OrderDetail.builder()
+                .orderDetailId(2L)
+                .itemOrderCnt(2)
+                .itemId(2L)
+                .itemName("짜장면")
+                .itemPrice(6000)
+                .build();
+    }
+
     static OrderDetailPostDto createPostDto1() {
         return OrderDetailPostDto.builder()
                 .itemId(1L)
